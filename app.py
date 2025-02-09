9e22fe12db07876426877c288502aa00a54e1125
quynnh-an <quynhanit@gmail.com>
2025-02-08 23:49:44 -0500

    error: name not found

--- app.py
+++ app.py
@@ -68,17 +68,6 @@ def waiting():
 def pre_result():
     return render_template('pre_result.html')
 
-@app.route('/result')
-def result():
-    # Read names from data.txt
-    names = []
-    with open('data.txt', 'r') as f:
-        for line in f:
-            user_data = json.loads(line)
-            names.append(user_data['name'])
-    
-    return render_template('result.html', names=names)
-
 @app.route('/check_result')
 def check_result():
     result_file = 'result.txt'
@@ -89,6 +78,71 @@ def check_result():
     else:
         return jsonify(hasResult=False)
 
+@app.route('/host')
+def host():
+    return render_template('host.html')
+
+@app.route('/result')
+def result():
+    # Get name from query parameter
+    name = request.args.get('name')
+    if not name:
+        return redirect(url_for('pre_result'))
+
+    # Read names for floating animation and check existence
+    names = []
+    name_exists = False
+    with open('data.txt', 'r') as f:
+        for line in f:
+            user_data = json.loads(line)
+            names.append(user_data['name'])
+            if user_data['name'] == name:
+                name_exists = True
+
+    if not name_exists:
+        return render_template('pre_result.html', error="Name not found. Please re-enter your name.")
+
+    # Search for match in result.txt
+    match_name = ""
+    match_discord = ""
+    is_round_2 = False
+    no_match = False
+
+    with open('result.txt', 'r') as f:
+        for line in f:
+            if name in line:
+                if "No match found" in line:
+                    no_match = True
+                    break
+                    
+                parts = line.strip().split(' - ')
+                if "Round 2" in parts[0]:
+                    is_round_2 = True
+                    
+                # Find the match name and discord
+                if parts[1].split(' (')[0] == name:
+                    match_name = parts[2].split(' (')[0]
+                    match_discord = parts[2].split('(')[1].rstrip(')')
+                else:
+                    match_name = parts[1].split(' (')[0]
+                    match_discord = parts[1].split('(')[1].rstrip(')')
+                break
+
+    if no_match:
+        return render_template('result.html', 
+                             match_name=False, 
+                             names=names)
+    elif is_round_2:
+        return render_template('result.html', 
+                             match_name=match_name,
+                             match_discord=match_discord,
+                             round_2_message="We're sorry that some of your preferences might not be satisfied due to gender ratio",
+                             names=names)
+    else:
+        return render_template('result.html',
+                             match_name=match_name, 
+                             match_discord=match_discord,
+                             names=names)
 
 if __name__ == '__main__':
     app.run(debug=True)
