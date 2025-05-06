83898c72d358b3aa01f967742903c14bbf087057
An Tran <quynhanit@gmail.com>
2025-05-06 12:07:36 -0400

    js bug

--- client/js/host.js
+++ client/js/host.js
@@ -4,7 +4,7 @@ function checkPassword() {
     .then(response => response.json())
     .then(data => {
       if (password === data.password) {
-        window.location.href = '/host-options';
+        window.location.href = '../host-options';
       } else {
         alert("Incorrect password. Please try again.");
       }
